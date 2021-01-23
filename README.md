9b98f0de86e76ad58e61d04a835e097198557000
adamallo <dmalload@asu.edu>
2021-01-22 18:27:30 -0700

    Version 1.1: Added option to obtain the breadth of coverage at n depths, and improved (only partially) the histogram options.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # genomedepth
 Small C program to efficiently generate summary statistics of sequencing depth and gap size. It uses the result of *bedtools genomecov -bga -ibam* as input.
+
+##Memory usage
+This program has been optimized for speed, not RAM usage. This grows linearly with the number of genomic positions and with the number of gaps. The main components that use ram should not use more than sizeof(int)*npos*1.5.
+
+##Example
+Using a bed file to subset a bam file efficiently (removing duplicates and secondary alignments) using samtools, and generating the input coverage bedgraph with bedtools. The bedgraph needs to be intersected again with the bed file, since samtools includes all overlapping read pairs, even if they don't align with the desired regions directly.
+
+```
+samtoolsview -F 1280 -u -h -b -L test.bed -M test.bam | bedtools genomecov -bga -ibam - | bedtools intersect -a - -b test.bed | genomedepth -g gap.histo -d depth.histo -r 100 -o test_genomedepth.out 
+```
--- src/main.c
+++ src/main.c
@@ -10,6 +10,15 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <limits.h>
+
+// Expands to an integer constant expression evaluating to a close upper bound
+// on the number the number of decimal digits in a value expressible in the
+// integer type given by the argument (if it is a type name) or the the integer
+// type of the argument (if it is an expression). The meaning of the resulting
+// expression is unspecified for other arguments.
+// By John Bollinger @ https://stackoverflow.com/questions/43787672/the-max-number-of-digits-in-an-int-based-on-number-of-bits
+#define DECIMAL_DIGITS_BOUND(t) (241 * sizeof(t) / 100 + 1)
 
 const int MEMBLOCK=1000000; //Size of the memory block added to the growing arrays
 
@@ -91,9 +100,9 @@ void write_histogram (struct hist_data histogram, FILE * file)
     long i=0;
     for (i=0; i<histogram.nbins-1;++i)
     {
-        fprintf(file,"[%f,%f),%ld\n",histogram.min+histogram.binsize*i,histogram.min+histogram.binsize*(i+1),*(histogram.counts+i));
+        fprintf(file,"[%.2f,%.2f),%ld\n",histogram.min+histogram.binsize*i,histogram.min+histogram.binsize*(i+1),*(histogram.counts+i));
     }
-    fprintf(file,"[%f,%f],%ld\n",histogram.min+histogram.binsize*(histogram.nbins-1),(double)histogram.max,*(histogram.counts+histogram.nbins-1));
+    fprintf(file,"[%.2f,%.2f],%ld\n",histogram.min+histogram.binsize*(histogram.nbins-1),(double)histogram.max,*(histogram.counts+histogram.nbins-1));
 }
 
 // Compare function by balki; https://stackoverflow.com/questions/49834742/how-to-write-qsort-comparison-function-for-integers
@@ -127,6 +136,19 @@ double imean (int *values,long nvalues)
     return total*1.0/nvalues;
 }
 
+//My total
+
+long long itot (int *values,long nvalues)
+{
+    long long total=0;
+    long i=0;
+    for (i=0; i<nvalues; ++i)
+    {
+        total+=values[i];
+    }
+    return total;
+}
+
 //My median with pre-sorted values
 double imedian (int *values, long nvalues)
 {
@@ -143,13 +165,15 @@ double imedian (int *values, long nvalues)
 //Main program
 int main(int argc, const char * argv[]) {
 
-    char *usage="genomedepth [-i input] [-o output] [-r number_of_histogram_ranges] [-d histogram_outfile]\nThis program parses the tsv file resulting from bedtools genomecov -bga -ibam. It uses stdin and stdout for input and output by default\n";
+    char *usage="genomedepth [-i input] [-o output] [-r number_of_histogram_ranges] [-g gap_histogram_outfile] [-d depth_histogram_outfile] [-b breadth_at_depth] [-b breadth_at_depth]\nThis program parses the bedgraph file resulting from bedtools genomecov -bga. It uses stdin and stdout for input and output by default\n";
     FILE *outmessage=stderr;
     
     //Defaults
     FILE *ifile=stdin;
     FILE *ofile=stdout;
-    FILE *hfile=NULL;
+    FILE *ghfile=NULL;
+    FILE *dhfile=NULL;
+
     int nbins=20;
     
     //ARGV parsing
@@ -157,8 +181,13 @@ int main(int argc, const char * argv[]) {
     char code=0; //Argument code (i.e., i for input, o for output)
     char *ifilename=NULL;
     char *ofilename=NULL;
-    char *hfilename=NULL;
+    char *ghfilename=NULL;
+    char *dhfilename=NULL;
     int arg=0;
+    int nbreadths=0;
+    struct di_array *breadths=calloc(sizeof(*breadths),1);
+    int parseBreadth;
+    
     for (arg=1; arg<argc;++arg)
     {
         //First character of the argument
@@ -189,13 +218,26 @@ int main(int argc, const char * argv[]) {
                 }
                 case 'D':
                 {
-                    hfilename=calloc(strlen(argv[arg])+1,sizeof(*hfilename));
-                    sscanf(argv[arg],"%s",hfilename);
+                    dhfilename=calloc(strlen(argv[arg])+1,sizeof(*dhfilename));
+                    sscanf(argv[arg],"%s",dhfilename);
+                    break;
+                }
+                case 'G':
+                {
+                    ghfilename=calloc(strlen(argv[arg])+1,sizeof(*ghfilename));
+                    sscanf(argv[arg],"%s",ghfilename);
                     break;
                 }
                 case 'R':
                 {
                     sscanf(argv[arg],"%d",&nbins);
+                    break;
+                }
+                case 'B':
+                {
+                    sscanf(argv[arg],"%d",&parseBreadth);
+                    addvalue(breadths,parseBreadth);
+                    ++nbreadths;
                     break;
                 }
                 default:
@@ -236,44 +278,103 @@ int main(int argc, const char * argv[]) {
         
         outmessage=stdout; //If we are not writting the results to stdout, we can use it to write messages as a log
     }
-    if (hfilename!=NULL)
+    if (ghfilename!=NULL)
     {
-        hfile=fopen(hfilename, "w");
+        ghfile=fopen(ghfilename, "w");
         
-        if (hfile==NULL)
+        if (ghfile==NULL)
         {
-            fprintf(stderr,"ERROR opening the output histogram file %s\n%s",ofilename,usage);
+            fprintf(stderr,"ERROR opening the output gap histogram file %s\n%s",ghfilename,usage);
+            return -1;
+        }
+    }
+    if (dhfilename!=NULL)
+    {
+        dhfile=fopen(dhfilename, "w");
+        
+        if (dhfile==NULL)
+        {
+            fprintf(stderr,"ERROR opening the output depth histogram file %s\n%s",dhfilename,usage);
             return -1;
         }
     }
     
     //Information on the run before actually starting it
-    fprintf(outmessage,"Genomedepth 1.0: calculating sequencing depth and gap statistics efficiently\n");
-    fprintf(outmessage,"\tInput file: %s\n\tOutput file: %s\n\tHistograms file: %s\n\tHistogram ranges: %d\n\n",ifilename==NULL?"stdin":ifilename,ofilename==NULL?"stdout":ofilename,hfilename==NULL?"Disabled":hfilename,nbins);
+    fprintf(outmessage,"\n############################################################################\nGenomedepth 1.1: calculating sequencing depth and gap statistics efficiently\n############################################################################\n\n");
+    
+    //Histogram information
+    char ghistogram_info[1000];//Not worth spending time on making this dynamic
+    if(ghfilename==NULL)
+    {
+        sprintf(ghistogram_info,"Gap histogram: disabled");
+    }
+    else
+    {
+        sprintf(ghistogram_info,"Gap histograms file: %s\nGap histogram ranges: %d",ghfilename,nbins);
+    }
+    
+    //Histogram information
+    char dhistogram_info[1000];//Not worth spending time on making this dynamic
+    if(dhfilename==NULL)
+    {
+        sprintf(dhistogram_info,"Depth histogram: disabled");
+    }
+    else
+    {
+        sprintf(dhistogram_info,"Depth histograms file: %s\nDepth histogram ranges: %d",dhfilename,nbins);
+    }
+    
+    //Breadths information
+    char* breadths_info=malloc(((DECIMAL_DIGITS_BOUND(int)+2)*nbreadths+12)*sizeof(char));//Each breadth can be a maximum of int length + , and  to separate them. We add 12 characters for \tBreadths: \0
+    char thisbreadth[DECIMAL_DIGITS_BOUND(int)+9]; //This should be +2, for the comma and the space. However, I am going to reuse this variable later, and I need +9 there
+    
+    int i=0;
+    if(nbreadths==0)
+    {
+        sprintf(breadths_info,"Breadths: disabled");
+    }
+    else
+    {
+        sprintf(breadths_info,"Breadths: %d",*breadths->mem);
+        for (i=1; i<nbreadths;++i)
+        {//working here
+            sprintf(thisbreadth,", %d",*(breadths->mem+i));
+            strcat(breadths_info, thisbreadth);
+        }
+    }
+    fprintf(outmessage,"Input file: %s\nOutput file: %s\n%s\n%s\n%s\n\n",ifilename==NULL?"stdin":ifilename,ofilename==NULL?"stdout":ofilename,ghistogram_info,dhistogram_info,breadths_info);
     
     //Variables for input file parsing
     ssize_t nchar=0;
     char *line=NULL;
     size_t lengthlineptr=0;
-    int spos=0,fpos=0,depth=0;
+    int spos=0,fpos=0;
+    long depth=0;
     char *chr=malloc(sizeof(*chr)*50);
     
     //Variables to collect input data
     struct di_array *adepths=calloc(sizeof(*adepths),1);
     struct di_array *agaps=calloc(sizeof(*agaps),1);
     long npos=0,ngaps=0,emptybases=0;
-    int posthissection=0,length=0;
+    int posthissection=0,length=0,ibreadth=0;
+    long *countBreadths;
+    countBreadths=calloc(nbreadths, sizeof(long));
     
     //Loop reading input file
     do
     {
         if (nchar>0) //skips empty lines
         {
-            sscanf(line,"%s\t%d\t%d\t%d\n",chr,&spos,&fpos,&depth);
+            sscanf(line,"%s\t%d\t%d\t%ld\n",chr,&spos,&fpos,&depth);
+            if(depth>INT_MAX)
+            {
+                fprintf(outmessage,"Error, the depth in this position is larger than the maximum integer number. This program needs to be re-compiled with unsigned integers or longs.");
+                exit(EXIT_FAILURE);
+            }
             length=fpos-spos;
             for (posthissection=0;posthissection<length;++posthissection)
             {
-                addvalue(adepths,depth);
+                addvalue(adepths,(int)depth);
                 ++npos;
             }
             if(depth==0)
@@ -281,6 +382,13 @@ int main(int argc, const char * argv[]) {
                 addvalue(agaps,length);
                 ++ngaps;
                 emptybases+=length;
+            }
+            for (ibreadth=0; ibreadth<nbreadths; ++ibreadth)
+            {
+                if((int)depth>=*(breadths->mem+ibreadth))
+                {
+                    countBreadths[ibreadth]+=length;
+                }
             }
         }
         nchar=getline(&line,&lengthlineptr,ifile);
@@ -292,6 +400,7 @@ int main(int argc, const char * argv[]) {
     qsort(agaps->mem, ngaps, sizeof(*agaps->mem),&compare);
     
     //Calculating mean and median
+    long long totalbases=itot(adepths->mem,npos);
     double meandepth=imean(adepths->mem,npos);
     double meandepthcovered=imean(adepths->mem+emptybases,npos-emptybases);
     double meangapsize=imean(agaps->mem,ngaps);
@@ -299,23 +408,59 @@ int main(int argc, const char * argv[]) {
     double mediandepth=imedian(adepths->mem, npos);
     double mediandepthcovered=imedian(adepths->mem+emptybases,npos-emptybases);
     double mediangapsize=imedian(agaps->mem,ngaps);
-    fprintf(ofile,"GenomeSize,Breadth1x,MeanDepth,MeanDepthCovered,MeanGapSize,MedianDepth,MedianDepthCovered,MedianGapSize\n%ld,%f,%f,%f,%f,%f,%f,%f\n",npos,100-(emptybases*100.0/npos),meandepth,meandepthcovered,meangapsize,mediandepth,mediandepthcovered,mediangapsize);
+    
+    //Breadths output information
+    char* breadths_output_header=malloc(((DECIMAL_DIGITS_BOUND(int)+9)*nbreadths+1)*sizeof(char));//Each breadth can be a maximum of int length + ,Breadthx; +1 for the \0
+    char* breadths_output_data=malloc((6*nbreadths+1)*sizeof(char));//Each breadth can be a maximum of ,100.0 + one character for \0
+    
+    
+    if(nbreadths==0)
+    {
+        sprintf(breadths_output_data,"");
+        sprintf(breadths_output_header,"");
+    }
+    else
+    {
+        sprintf(breadths_output_header,"Breadth%dx",*breadths->mem);
+        sprintf(breadths_output_data,"%.2f",*countBreadths*100.0/npos);
+        for (i=1; i<nbreadths;++i)
+        {//working here
+            sprintf(thisbreadth,",Breadth%dx",*(breadths->mem+i));
+            strcat(breadths_output_header,thisbreadth);
+            sprintf(thisbreadth,",%.2f",*(countBreadths+i)*100.0/npos);
+            strcat(breadths_output_data,thisbreadth);
+        }
+    }
+    
+    fprintf(ofile,"GenomeSize,TotalBases,Breadth1x,%s,MeanDepth,MeanDepthCovered,MeanGapSize,MedianDepth,MedianDepthCovered,MedianGapSize\n%ld,%lld,%.2f,%s,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\n",breadths_output_header,npos,totalbases,100-(emptybases*100.0/npos),breadths_output_data,meandepth,meandepthcovered,meangapsize,mediandepth,mediandepthcovered,mediangapsize);
     //Calculating histograms
     
-    if(hfile!=NULL)
+    if(ghfile!=NULL)
     {
-        //struct hist_data depth_hist=histogram(adepths->mem,npos,nbins);
         struct hist_data gap_hist=histogram_nbins(agaps->mem,ngaps,nbins);
         //Printing histograms
-        write_histogram(gap_hist,hfile);
+        write_histogram(gap_hist,ghfile);
     }
+    
+    if(dhfile!=NULL)
+    {
+        struct hist_data depth_hist=histogram_nbins(adepths->mem,npos,nbins);
+        //Printing histograms
+        write_histogram(depth_hist,dhfile);
+    }
+
     //Freeing memory, closing files and goodbye
     free(chr);
+    free(breadths_info);
+    free(countBreadths);
+    free(breadths_output_header);
+    free(breadths_output_data);
     if (ifilename!=NULL) free(ifilename);
     if (ofilename!=NULL) free(ofilename);
     fclose(ifile);
     fclose(ofile);
-    if (hfile!=NULL) fclose (hfile);
+    if (dhfile!=NULL) fclose (dhfile);
+    if (ghfile!=NULL) fclose (ghfile);
     free_di_array(agaps);
     free_di_array(adepths);
     return 0;
